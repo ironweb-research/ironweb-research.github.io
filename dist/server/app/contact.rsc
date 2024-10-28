c4c58065106ffa84eeac95a1afbe0184d44dc89a
ironweb-research <backslash.with.n@gmail.com>
2024-10-28 16:27:56 +0800

    try out NextJS as homepage using README.md

---
+++ dist/server/app/contact.rsc
@@ -0,0 +1,91 @@
+1:"$Sreact.fragment"
+2:I[9275,[],""]
+3:I[1343,[],""]
+5:I[3120,[],"OutletBoundary"]
+7:I[3120,[],"MetadataBoundary"]
+9:I[3120,[],"ViewportBoundary"]
+b:I[6130,[],""]
+4:T620,
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      0:{"P":null,"b":"WZBF40yohlEHtdGCwFmEi","p":"","c":["","contact"],"i":false,"f":[[["",{"children":["contact",{"children":["__PAGE__",{}]}]},"$undefined","$undefined",true],["",["$","$1","c",{"children":[null,["$","html",null,{"lang":"en","children":["$","body",null,{"children":["$","$L2",null,{"parallelRouterKey":"children","segmentPath":["children"],"error":"$undefined","errorStyles":"$undefined","errorScripts":"$undefined","template":["$","$L3",null,{}],"templateStyles":"$undefined","templateScripts":"$undefined","notFound":[["$","title",null,{"children":"404: This page could not be found."}],["$","div",null,{"style":{"fontFamily":"system-ui,\"Segoe UI\",Roboto,Helvetica,Arial,sans-serif,\"Apple Color Emoji\",\"Segoe UI Emoji\"","height":"100vh","textAlign":"center","display":"flex","flexDirection":"column","alignItems":"center","justifyContent":"center"},"children":["$","div",null,{"children":[["$","style",null,{"dangerouslySetInnerHTML":{"__html":"body{color:#000;background:#fff;margin:0}.next-error-h1{border-right:1px solid rgba(0,0,0,.3)}@media (prefers-color-scheme:dark){body{color:#fff;background:#000}.next-error-h1{border-right:1px solid rgba(255,255,255,.3)}}"}}],["$","h1",null,{"className":"next-error-h1","style":{"display":"inline-block","margin":"0 20px 0 0","padding":"0 23px 0 0","fontSize":24,"fontWeight":500,"verticalAlign":"top","lineHeight":"49px"},"children":"404"}],["$","div",null,{"style":{"display":"inline-block"},"children":["$","h2",null,{"style":{"fontSize":14,"fontWeight":400,"lineHeight":"49px","margin":0},"children":"This page could not be found."}]}]]}]}]],"notFoundStyles":[]}]}]}]]}],{"children":["contact",["$","$1","c",{"children":[null,["$","$L2",null,{"parallelRouterKey":"children","segmentPath":["children","contact","children"],"error":"$undefined","errorStyles":"$undefined","errorScripts":"$undefined","template":["$","$L3",null,{}],"templateStyles":"$undefined","templateScripts":"$undefined","notFound":"$undefined","notFoundStyles":"$undefined"}]]}],{"children":["__PAGE__",["$","$1","c",{"children":[["$","div",null,{"className":"container","children":[["$","main",null,{"children":[["$","h1",null,{"className":"title","children":["Welcome to ",["$","a",null,{"href":"https://nextjs.org","children":"Next.js!"}]]}],["$","p",null,{"className":"description","children":"Contact Page"}]]}],["$","footer",null,{"children":["$","a",null,{"href":"https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app","target":"_blank","rel":"noopener noreferrer","children":["Powered by"," ",["$","img",null,{"src":"/vercel.svg","alt":"Vercel Logo","className":"logo"}]]}]}],["$","style",null,{"children":"$4"}],["$","style",null,{"children":"\n        html,\n        body {\n          padding: 0;\n          margin: 0;\n          font-family:\n            -apple-system,\n            BlinkMacSystemFont,\n            Segoe UI,\n            Roboto,\n            Oxygen,\n            Ubuntu,\n            Cantarell,\n            Fira Sans,\n            Droid Sans,\n            Helvetica Neue,\n            sans-serif;\n        }\n\n        * {\n          box-sizing: border-box;\n        }\n      "}]]}],null,["$","$L5",null,{"children":"$L6"}]]}],{},null]},null]},null],["$","$1","h",{"children":[null,["$","$1","Md9Jjpw1XNb-NjRazYvMO",{"children":[["$","$L7",null,{"children":"$L8"}],["$","$L9",null,{"children":"$La"}],null]}]]}]]],"m":"$undefined","G":"$b","s":false,"S":true}
+a:[["$","meta","0",{"name":"viewport","content":"width=device-width, initial-scale=1"}]]
+8:[["$","meta","0",{"charSet":"utf-8"}],["$","title","1",{"children":"Create Next App"}],["$","meta","2",{"name":"description","content":"Generated by Next.js"}],["$","link","3",{"rel":"icon","href":"/favicon.ico"}]]
+6:null
---
+++ dist/server/app/expertises/page.js
@@ -0,0 +1,161 @@
+(()=>{var e={};e.id=376,e.ids=[376],e.modules={399:e=>{"use strict";e.exports=require("next/dist/compiled/next-server/app-page.runtime.prod.js")},209:e=>{"use strict";e.exports=require("next/dist/server/app-render/action-async-storage.external.js")},9348:e=>{"use strict";e.exports=require("next/dist/server/app-render/work-async-storage.external.js")},412:e=>{"use strict";e.exports=require("next/dist/server/app-render/work-unit-async-storage.external.js")},5315:e=>{"use strict";e.exports=require("path")},359:(e,r,t)=>{"use strict";t.r(r),t.d(r,{GlobalError:()=>a.a,__next_app__:()=>p,pages:()=>c,routeModule:()=>m,tree:()=>d});var i=t(3003),n=t(4293),o=t(6550),a=t.n(o),s=t(6979),l={};for(let e in s)0>["default","tree","pages","GlobalError","__next_app__","routeModule"].indexOf(e)&&(l[e]=()=>s[e]);t.d(r,l);let d=["",{children:["expertises",{children:["__PAGE__",{},{page:[()=>Promise.resolve().then(t.bind(t,2635)),"D:\\kht\\1_Project\\3_ironweb\\myhomepage\\app\\expertises\\page.tsx"]}]},{}]},{layout:[()=>Promise.resolve().then(t.bind(t,6877)),"D:\\kht\\1_Project\\3_ironweb\\myhomepage\\app\\layout.tsx"],"not-found":[()=>Promise.resolve().then(t.t.bind(t,2075,23)),"next/dist/client/components/not-found-error"]}],c=["D:\\kht\\1_Project\\3_ironweb\\myhomepage\\app\\expertises\\page.tsx"],p={require:t,loadChunk:()=>Promise.resolve()},m=new i.AppPageRouteModule({definition:{kind:n.RouteKind.APP_PAGE,page:"/expertises/page",pathname:"/expertises",bundlePath:"",filename:"",appPaths:[]},userland:{loaderTree:d}})},6037:(e,r,t)=>{Promise.resolve().then(t.t.bind(t,6114,23)),Promise.resolve().then(t.t.bind(t,2639,23)),Promise.resolve().then(t.t.bind(t,9727,23)),Promise.resolve().then(t.t.bind(t,9671,23)),Promise.resolve().then(t.t.bind(t,1868,23)),Promise.resolve().then(t.t.bind(t,4759,23)),Promise.resolve().then(t.t.bind(t,2816,23))},5303:()=>{},2635:(e,r,t)=>{"use strict";t.r(r),t.d(r,{default:()=>n});var i=t(9351);function n(){return(0,i.jsxs)("div",{className:"container",children:[(0,i.jsxs)("main",{children:[(0,i.jsx)("h1",{className:"title",children:"Our Expertises"}),(0,i.jsx)("p",{className:"description",children:"We are a start-up research company where we dream about and develop the next-gen web security suite"})]}),(0,i.jsx)("footer",{children:(0,i.jsxs)("a",{href:"https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app",target:"_blank",rel:"noopener noreferrer",children:["Powered by"," ",(0,i.jsx)("img",{src:"/vercel.svg",alt:"Vercel Logo",className:"logo"})]})}),(0,i.jsx)("style",{children:`
+        .container {
+          min-height: 100vh;
+          padding: 0 0.5rem;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        main {
+          padding: 5rem 0;
+          flex: 1;
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer {
+          width: 100%;
+          height: 100px;
+          border-top: 1px solid #eaeaea;
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        footer img {
+          margin-left: 0.5rem;
+        }
+
+        footer a {
+          display: flex;
+          justify-content: center;
+          align-items: center;
+        }
+
+        a {
+          color: inherit;
+          text-decoration: none;
+        }
+
+        .title a {
+          color: #0070f3;
+          text-decoration: none;
+        }
+
+        .title a:hover,
+        .title a:focus,
+        .title a:active {
+          text-decoration: underline;
+        }
+
+        .title {
+          margin: 0;
+          line-height: 1.15;
+          font-size: 4rem;
+        }
+
+        .title,
+        .description {
+          text-align: center;
+        }
+
+        .description {
+          line-height: 1.5;
+          font-size: 1.5rem;
+        }
+
+        code {
+          background: #fafafa;
+          border-radius: 5px;
+          padding: 0.75rem;
+          font-size: 1.1rem;
+          font-family:
+            Menlo,
+            Monaco,
+            Lucida Console,
+            Liberation Mono,
+            DejaVu Sans Mono,
+            Bitstream Vera Sans Mono,
+            Courier New,
+            monospace;
+        }
+
+        .grid {
+          display: flex;
+          align-items: center;
+          justify-content: center;
+          flex-wrap: wrap;
+
+          max-width: 800px;
+          margin-top: 3rem;
+        }
+
+        .card {
+          margin: 1rem;
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition:
+            color 0.15s ease,
+            border-color 0.15s ease;
+        }
+
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+
+        .logo {
+          height: 1em;
+        }
+
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}),(0,i.jsx)("style",{children:`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+
+        * {
+          box-sizing: border-box;
+        }
+      `})]})}},6877:(e,r,t)=>{"use strict";t.r(r),t.d(r,{default:()=>o,metadata:()=>n});var i=t(9351);let n={title:"Create Next App",description:"Generated by Next.js",icons:{icon:"/favicon.ico"}};function o({children:e}){return(0,i.jsx)("html",{lang:"en",children:(0,i.jsx)("body",{children:e})})}}};var r=require("../../webpack-runtime.js");r.C(e);var t=e=>r(r.s=e),i=r.X(0,[147,828],()=>t(359));module.exports=i})();
