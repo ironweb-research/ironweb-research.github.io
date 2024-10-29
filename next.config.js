755d0e34d90184707943fe0cbf60a9896debd21d
ironweb-research <backslash.with.n@gmail.com>
2024-10-29 11:32:35 +0800

    remove output: 'export' from next.config.js file

--- next.config.js
+++ next.config.js
@@ -1,20 +1,20 @@
-// /** @type {import('next').NextConfig} */
-// module.exports = {};
+/** @type {import('next').NextConfig} */
+module.exports = {};
 
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-    output: 'export',
+// /**
+//  * @type {import('next').NextConfig}
+//  */
+// const nextConfig = {
+//     output: 'export',
    
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    // trailingSlash: true,
+//     // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+//     // trailingSlash: true,
    
-    // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-    // skipTrailingSlashRedirect: true,
+//     // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+//     // skipTrailingSlashRedirect: true,
    
-    // Optional: Change the output directory `out` -> `dist`
-    // distDir: 'dist',
-  }
+//     // Optional: Change the output directory `out` -> `dist`
+//     // distDir: 'dist',
+//   }
    
-  module.exports = nextConfig
+//   module.exports = nextConfig
