0294ba45bcc13180490d23483a4ba88814bb2e5c
ironweb-research <backslash.with.n@gmail.com>
2024-11-01 08:45:08 +0800

    clean up sitemap items

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,3 +1,5 @@
+import { url } from "inspector";
+
 const globby = require("globby");
 
 export const revalidate = 3600; // hourly revalidation
@@ -19,14 +21,29 @@ export default async function sitemap() {
     "!app/_*.js",
     "!app/{sitemap,layout}.{js,jsx,ts,tsx}",
     "!app/api",
+    "!app/_components/*{.js,jsx,ts,tsx,.mdx}",
+    "!app/lib/*{.js,jsx,ts,tsx,.mdx}"
   ]);
   const routes = pages.map((page: string) => ({
     url: `${process.env.WEBSITE_URL}${addPage(page)}`,
     lastModified: new Date().toISOString(),
   }));
   
-  console.log(`routes                        >>>>>>>>>>>>>>>>: ${{routes}}`)
+  console.log(`routes                        >>>>>>>>>>>>>>>>: ${{routes}.routes}`)
   console.log(`pages                         >>>>>>>>>>>>>>>>: ${pages}`)
   console.log(`process.env.WEBSITE_URL       >>>>>>>>>>>>>>>>: ${process.env.WEBSITE_URL}`)
+  console.log(`process.env.WEBSITE_URL       >>>>>>>>>>>>>>>>: ${{url}}`)
   return [...routes]; // spread operator on [routes]
 }
+/* Only sitemap.ts outputs its console.log, as shown below:
+        generate-sitemap.js does not output its console.log
+route = page.                  >>>>>>>>>>>>>>>: app/_components/theme-switcher.tsx
+route = path.                  >>>>>>>>>>>>>>>: _components/theme-switcher
+routes                        >>>>>>>>>>>>>>>>: [object Object]
+pages                         >>>>>>>>>>>>>>>>: app/page.tsx,app/contact/page.tsx, \
+  app/expertises/page.tsx,app/lib/constants.ts,app/roots/page.tsx,app/_components/alert.tsx, \
+  app/_components/avatar.tsx,app/_components/container.tsx,app/_components/cover-image.tsx, \
+  app/_components/date-formatter.tsx,app/_components/footer.tsx,app/_components/header.tsx, \
+  app/_components/intro.tsx,app/_components/section-separator.tsx,app/_components/theme-switcher.tsx
+process.env.WEBSITE_URL       >>>>>>>>>>>>>>>>: https://ironweb-research.github.io/
+*/
--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -1,9 +1,34 @@
 const fs = require("fs");
 const globby = require("globby");
 
+
+/* Only sitemap.ts outputs its console.log, as shown below:
+        generate-sitemap.js does not output its console.log
+route = page.                  >>>>>>>>>>>>>>>: app/_components/theme-switcher.tsx
+route = path.                  >>>>>>>>>>>>>>>: _components/theme-switcher
+routes                        >>>>>>>>>>>>>>>>: [object Object]
+pages                         >>>>>>>>>>>>>>>>: app/page.tsx,app/contact/page.tsx, \
+  app/expertises/page.tsx,app/lib/constants.ts,app/roots/page.tsx,app/_components/alert.tsx, \
+  app/_components/avatar.tsx,app/_components/container.tsx,app/_components/cover-image.tsx, \
+  app/_components/date-formatter.tsx,app/_components/footer.tsx,app/_components/header.tsx, \
+  app/_components/intro.tsx,app/_components/section-separator.tsx,app/_components/theme-switcher.tsx
+process.env.WEBSITE_URL       >>>>>>>>>>>>>>>>: https://ironweb-research.github.io/
+*/
+
 function addPage(page) {
+
+  // route = page.                  >>>>>>>>>>>>>>>: app/_components/theme-switcher.tsx
+  // route = path.                  >>>>>>>>>>>>>>>: _components/theme-switcher
+
   const path = page.replace("pages", "").replace(".js", "").replace(".mdx", "");
+
+// path = page = app/_components/theme-switcher.tsx
+// tsx file survives!
+
   const route = path === "/index" ? "" : path;
+
+// route = app/_components/theme-switcher.tsx
+
   console.log(`gen_sitemap____route = path.                  >>>>>>>>>>>>>>>>: ${path}`)
   console.log(`gen_sitemap____process.env.WEBSITE_URL_route. >>>>>>>>>>>>>>>>: ${process.env.WEBSITE_URL}${route}`)
 
@@ -12,6 +37,8 @@ function addPage(page) {
     <changefreq>hourly</changefreq>
   </url>`;
 }
+// https://ironweb-research.github.io/expertises
+// https://ironweb-research.github.io/app/_components/theme-switcher.tsx
 
 async function generateSitemap() {
   // Ignore Next.js specific files (e.g., _app.js) and API routes.
