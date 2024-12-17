1252fe76699146b58a6e79768f8b1f2e7a9f0345
ironweb-research <backslash.with.n@gmail.com>
2024-12-17 18:04:31 +0800

    create [main_frame] folder under _components to enable pulling call from website_hosting / we_are

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Container from "@/app/_components/container";
-import Alert from "@/app/_components/alert";
-import { Intro } from "@/app/_components/intro";
+import Alert from "@/app/_components/main_frame/alert";
+import { Intro } from "@/app/_components/main_frame/intro";
 import CoverPage from "@/app/_components/coverpage";
 
 export default function Index() {
